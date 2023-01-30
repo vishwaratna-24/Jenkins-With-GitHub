d4d40151363f0d893b981bffe60e629a2d3007bb
vishw <vishw@VISHWA>
2023-01-30 13:36:45 +0530

    Add project files.

---
+++ Jenkins-With-GitHub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jenkins_With_GitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
